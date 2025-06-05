1f752aa10c464ab758b98750415cdab0664daede
sauravs75 <sauravs95088@gmail.com>
2025-06-06 00:42:07 +0530

    first commit

---
+++ Demo3/src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./NavBar"
+import HeroSection from "./HeroSection"
+import Footer from "./Footer"
+
+function App() {
+
+
+  return (
+    <>
+    <NavBar/> 
+    <HeroSection/>
+    <Footer/> 
+    </>
+  )
+}
+
+export default App
